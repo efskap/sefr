3505b2ecdae870a5e38c944a67128bec2ff1fa4d
Dmitry Narkevich <me@dmitry.lol>
2019-05-13 14:21:57 +0000

    restructure (state now in guithread), implement tab-completion from suggestions

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 extern crate crossterm;
-extern crate reqwest;
 extern crate json;
+extern crate reqwest;
 extern crate webbrowser;
 
 use crossterm::{
-    input, Attribute, ClearType, Color, Colored, Crossterm,
-    InputEvent, KeyEvent, RawScreen, Terminal, TerminalCursor,
+    input, Attribute, ClearType, Color, Colored, Crossterm, InputEvent, KeyEvent, RawScreen,
 };
 
-use std::cmp::max;
+use std::cmp::min;
 use std::collections::HashMap;
 use std::result::Result;
 use std::sync::mpsc;
-use std::sync::mpsc::Receiver;
 use std::thread;
 
 #[allow(unused_must_use)]
@@ -21,68 +19,28 @@ fn main() {
 
     let _screen = RawScreen::into_raw_mode();
     let crossterm = Crossterm::new();
-    let cursor = crossterm.cursor();
+    let mut cursor = crossterm.cursor();
     let terminal = crossterm.terminal();
 
     let input = input();
     let mut stdin = input.read_sync();
     let mut input_buf = String::new();
 
+    let engines = define_engines();
+    let input_tx = tx.clone();
     let input_thread = thread::spawn(move || {
-        let engines = define_engines();
+        let mut should_quit = false;
         loop {
-            let (engine, search_term) = match_engine(&input_buf, &engines);
-            tx.send(UiMsg::SetInputLine(
-                engine.prompt.clone(),
-                input_buf.clone(),
-            ));
-            if input_buf.is_empty() {
-                tx.send(UiMsg::SetSuggestions(None));
-            } else {
-                tx.send(UiMsg::ExpectSuggsFor(search_term.clone()));
-                let url = engine.format_suggestion_url(&search_term);
-                let tx2 = tx.clone();
-                thread::spawn(move || {
-                    tx2.send(UiMsg::SetSuggestions(fetch_suggs(url).ok()));
-                });
-            }
-            match stdin.next() {
+            let key = stdin.next();
+            match &key {
                 Some(ie) => match ie {
                     InputEvent::Keyboard(k) => match k {
                         KeyEvent::Char('\n') => {
-                            input_buf.clear();
-                            let url = engine.format_search_url(&search_term);
-                            tx.send(UiMsg::Finish(format!(
-                                "=> {}", url
-                            )));
-                            webbrowser::open(&url).expect("Couldn't open browser.");
-                            break;
-                        }
-                        KeyEvent::Char('\t') | KeyEvent::Ctrl('n') | KeyEvent::Down => {
-                            tx.send(UiMsg::SelNext);
-                        }
-                        KeyEvent::BackTab | KeyEvent::Ctrl('p') | KeyEvent::Up => {
-                            tx.send(UiMsg::SelPrev);
-                        }
-                        KeyEvent::Char(character) => {
-                            input_buf.push(character as char);
-                        }
-
-                        KeyEvent::Backspace => {
-                            input_buf.pop();
+                            should_quit = true;
                         }
                         KeyEvent::Ctrl(c) => match c {
                             'c' => {
-                                tx.send(UiMsg::Quit);
-                                break;
-                            }
-                            'w' => {
-                                // delete last word (trim, then delete backwards until first
-                                // word character or beginning of line
-                                input_buf = input_buf
-                                    .trim_end()
-                                    .trim_end_matches(|x: char| !x.is_whitespace())
-                                    .to_string();
+                                should_quit = true;
                             }
                             _ => {}
                         },
@@ -92,13 +50,211 @@ fn main() {
                 },
                 _ => {}
             };
+            input_tx.send(UiMsg::OnInput(key));
+            if should_quit {
+                break;
+            }
         }
     });
-    let ui_thread = thread::spawn(move || {
-        ui_loop(rx, terminal, cursor);
-    });
+
+    cursor.hide();
+    let mut input_line: String = String::from("");
+    let mut suggs: Option<Suggestions> = None;
+    let mut prompt = &engines.get("").expect("No default engine set.").prompt;
+    let mut waiting_for_term: Option<String> = None; //
+    let (t_w, _t_h) = terminal.terminal_size();
+    let mut selected_n: Option<usize> = None;
+
+    let mut refresh_completions = true;
+    loop {
+        let (engine, prefix, search_term) = match_engine(&input_line, &engines);
+        if refresh_completions {
+            prompt = &engine.prompt;
+            if input_line.is_empty() {
+                suggs = None;
+            } else {
+                waiting_for_term = Some(search_term.clone());
+                let url = engine.format_suggestion_url(&search_term);
+                let tx2 = tx.clone();
+                thread::spawn(move || {
+                    if let Some(resolved_suggs) = fetch_suggs(url).ok() {
+                        tx2.send(UiMsg::SetSuggestions(resolved_suggs));
+                    }
+                });
+            }
+            refresh_completions = false;
+        }
+        cursor.move_left(t_w);
+        terminal.clear(ClearType::CurrentLine);
+        print!(
+            "{}{}{}{}",
+            Colored::Fg(prompt.icon_fg),
+            Colored::Bg(prompt.icon_bg),
+            prompt.icon,
+            Attribute::Reset
+        ); // icon
+        print!(
+            "{}{}{}{}",
+            Colored::Fg(prompt.text_fg),
+            Colored::Bg(prompt.text_bg),
+            prompt.text,
+            Attribute::Reset
+        ); // promptt text
+        println!(" {}_", input_line);
+        let suggest_lines = 15; /*if let Some(ref suggs) = suggs {
+                                    suggs.sugg_terms.len()
+                                } else {
+                                    0
+                                }; */
+        let selectable_lines = if let Some(ref suggs) = suggs { min(suggest_lines, suggs.sugg_terms.len())} else {0};
+        for n in 0..suggest_lines {
+            terminal.clear(ClearType::CurrentLine);
+            cursor.move_left(t_w);
+
+            if let Some(ref suggs) = suggs {
+                match suggs.sugg_terms.get(n) {
+                    Some(line) => match selected_n {
+                        Some(selected_n) if selected_n == n => {
+                            print!(
+                                "{}{}{}{}",
+                                Colored::Fg(Color::Black),
+                                Colored::Bg(Color::White),
+                                line,
+                                Attribute::Reset
+                            );
+                        }
+                        _ => {
+                            print!("{}", line);
+                        }
+                    },
+                    None => {} // outside sugg bounds
+                }
+            }
+
+            println!();
+        }
+        let msg = rx.recv().unwrap();
+        match msg {
+            UiMsg::SetSuggestions(suggestion_update) => {
+                if let Some(ref expected_term) = waiting_for_term {
+                    if &suggestion_update.term == expected_term {
+                        suggs = Some(suggestion_update);
+                    }
+                }
+            }
+            UiMsg::OnInput(key) => {
+                refresh_completions = true;
+                match key {
+                    Some(ie) => match ie {
+                        InputEvent::Keyboard(k) => match k {
+                            KeyEvent::Char('\n') => {
+                                input_buf.clear();
+                                let url = engine.format_search_url(&search_term);
+                                for _ in 0..suggest_lines {
+                                    cursor.move_up(1);
+                                    terminal.clear(ClearType::CurrentLine);
+                                }
+                                cursor.move_left(t_w);
+                                println!("Opening {}", url);
+                                terminal.clear(ClearType::CurrentLine);
+                                webbrowser::open(&url).expect("Couldn't open browser.");
+                                break;
+                            }
+                            // TODO: clean up brutal code reuse here
+                            KeyEvent::Char('\t') | KeyEvent::Ctrl('n') | KeyEvent::Down => {
+                                if let Some(ref suggs) = suggs {
+                                    selected_n = Some(if let Some(selected_n) = selected_n {
+                                        selected_n + 1
+                                    } else {
+                                        0
+                                    });
+                                    if selected_n.unwrap()
+                                        >= selectable_lines
+                                    {
+                                        selected_n = Some(0);
+                                    }
+                                    if let Some(selected) =
+                                        suggs.sugg_terms.get(selected_n.unwrap())
+                                    {
+                                        let (_, interfering_prefix, _) =
+                                            match_engine(&selected, &engines);
+                                        if !interfering_prefix.is_empty() {
+                                            input_line = format!("?{}", selected.to_string());
+                                        } else if !prefix.is_empty() {
+                                            input_line =
+                                                format!("{} {}", prefix, selected.to_string());
+                                        } else {
+                                            input_line = format!("{}", selected.to_string());
+                                        }
+                                        refresh_completions = false;
+                                    }
+                                }
+                            }
+                            KeyEvent::BackTab | KeyEvent::Ctrl('p') | KeyEvent::Up => {
+                                if let Some(ref suggs) = suggs {
+                                    selected_n =
+                                        Some(selected_n.unwrap_or(0).checked_sub(1).unwrap_or(
+                                           selectable_lines.checked_sub(1).unwrap_or(0),
+                                        ));
+                                    if let Some(selected) =
+                                        suggs.sugg_terms.get(selected_n.unwrap())
+                                    {
+                                        let (_, interfering_prefix, _) =
+                                            match_engine(&selected, &engines);
+                                        if !interfering_prefix.is_empty() {
+                                            input_line = format!("?{}", selected.to_string());
+                                        } else if !prefix.is_empty() {
+                                            input_line =
+                                                format!("{} {}", prefix, selected.to_string());
+                                        } else {
+                                            input_line = format!("{}", selected.to_string());
+                                        }
+                                        refresh_completions = false;
+                                    }
+                                }
+                            }
+                            KeyEvent::Char(character) => {
+                                input_line.push(character as char);
+                                selected_n = None;
+                            }
+
+                            KeyEvent::Backspace => {
+                                input_line.pop();
+                                selected_n = None;
+                            }
+                            KeyEvent::Ctrl(c) => match c {
+                                'c' => {
+                                    terminal.clear(ClearType::CurrentLine);
+                                    for _ in 0..suggest_lines {
+                                        cursor.move_up(1);
+                                        terminal.clear(ClearType::CurrentLine);
+                                    }
+                                    break;
+                                }
+                                'w' => {
+                                    // delete last word (trim, then delete backwards until first
+                                    // word character or beginning of line
+                                    input_line = input_line
+                                        .trim_end()
+                                        .trim_end_matches(|x: char| !x.is_whitespace())
+                                        .to_string();
+                                }
+                                _ => {}
+                            },
+                            _ => {}
+                        },
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            }
+        };
+        cursor.move_up(suggest_lines as u16 + 1);
+    }
+    cursor.show();
+    cursor.move_left(t_w);
+
     input_thread.join();
-    ui_thread.join();
 }
 
 fn fetch_suggs(url: String) -> Result<Suggestions, Box<std::error::Error>> {
@@ -122,38 +278,41 @@ struct Engine {
 
 impl Engine {
     pub fn format_suggestion_url(&self, search_term: &str) -> String {
-        self.suggestion_url.replace("%s", &search_term.replace(" ", "+"))
+        self.suggestion_url
+            .replace("%s", &search_term.replace(" ", "+"))
     }
     pub fn format_search_url(&self, search_term: &str) -> String {
-        self.search_url.replace("%s", &search_term.replace(" ", "+"))
+        self.search_url
+            .replace("%s", &search_term.replace(" ", "+"))
     }
 }
 
+// TODO: this should prolly return slices, not Strings
 fn match_engine<'a, 'b>(
     input_line: &'b str,
     engines: &'a HashMap<String, Engine>,
-) -> (&'a Engine, String) {
+) -> (&'a Engine, String, String) {
     let default_engine = engines.get("").unwrap();
 
     // escape search engine keyword with question mark like Chrome
     if input_line.starts_with("?") {
-        return (default_engine, input_line[1..].to_string());
+        return (default_engine, String::new(), input_line[1..].to_string());
     }
 
     let words: Vec<&str> = input_line.split_whitespace().collect();
 
     // in the empty case, or if a keyword is typed but there's no space after it, skip matching.
     if words.len() < 1 || (words.len() == 1 && !input_line.ends_with(" ")) {
-        return (default_engine, input_line.trim().to_string());
+        return (default_engine, String::new(), input_line.trim().to_string());
     }
 
     let potential_prefix = words.first().unwrap();
     match engines.get(&potential_prefix.to_string()) {
         Some(engine) => {
             let search_term = input_line[potential_prefix.len()..].trim().to_string();
-            (engine, search_term)
+            (engine, potential_prefix.to_string(), search_term)
         }
-        None => (default_engine, input_line.trim().to_string()),
+        None => (default_engine, String::new(), input_line.trim().to_string()),
     }
 }
 
@@ -265,123 +424,6 @@ struct Prompt {
 }
 
 enum UiMsg {
-    Quit,
-    Finish(String),
-    SetInputLine(Prompt, String),
-    SetSuggestions(Option<Suggestions>),
-    ExpectSuggsFor(String),
-    SelNext,
-    SelPrev,
-}
-
-#[allow(unused_must_use)]
-fn ui_loop(rx: Receiver<UiMsg>, terminal: Terminal, mut cursor: TerminalCursor) {
-    cursor.hide();
-    let mut input_line: String = String::from("");
-    let mut suggs: Option<Suggestions> = None;
-    let mut prompt: Option<Prompt> = None;
-    let mut waiting_for_term: Option<String> = None; //
-    let (t_w, _t_h) = terminal.terminal_size();
-    let mut selected_n: usize = 0;
-    loop {
-        cursor.move_left(t_w);
-        terminal.clear(ClearType::CurrentLine);
-        match &prompt {
-            Some(promp) => {
-                print!(
-                    "{}{}{}{}",
-                    Colored::Fg(promp.icon_fg),
-                    Colored::Bg(promp.icon_bg),
-                    promp.icon,
-                    Attribute::Reset
-                ); // icon
-                print!(
-                    "{}{}{}{}",
-                    Colored::Fg(promp.text_fg),
-                    Colored::Bg(promp.text_bg),
-                    promp.text,
-                    Attribute::Reset
-                ); // prompt text
-            }
-            None => {}
-        };
-        println!(" {}_", input_line);
-        let suggest_lines = 15; /*if let Some(ref suggs) = suggs {
-            suggs.sugg_terms.len()
-        } else {
-            0
-        }; */
-        for n in 0..suggest_lines {
-
-            terminal.clear(ClearType::CurrentLine);
-            cursor.move_left(t_w);
-
-            if let Some(ref suggs) = suggs {
-                match suggs.sugg_terms.get(n) {
-                    Some(line) => {
-                        if selected_n == n {
-                            //print!("-");
-                        }
-                        print!("{}", line);
-                    },
-                    None => {}
-                }
-            }
-
-            println!();
-        }
-        let msg = rx.recv().unwrap();
-        match msg {
-            UiMsg::Quit => {
-                terminal.clear(ClearType::CurrentLine);
-                for _ in 0..suggest_lines {
-                    cursor.move_up(1);
-                    terminal.clear(ClearType::CurrentLine);
-                }
-                break;
-            }
-            UiMsg::Finish(s) => {
-                for _ in 0..suggest_lines {
-                    cursor.move_up(1);
-                    terminal.clear(ClearType::CurrentLine);
-                }
-                cursor.move_left(t_w);
-                println!("{}", s);
-                terminal.clear(ClearType::CurrentLine);
-                break;
-            }
-            UiMsg::SetInputLine(new_prompt, s) => {
-                input_line = s;
-                prompt = Some(new_prompt);
-            }
-            UiMsg::SetSuggestions(suggestion_update) => {
-                if let Some(ref new_sugg) = suggestion_update {
-                    if let Some(ref expected_term) = waiting_for_term {
-                        if &new_sugg.term == expected_term {
-                            suggs = suggestion_update;
-                        }
-                    }
-                } else {
-                    suggs = None;
-                }
-            }
-            UiMsg::SelNext => {
-                selected_n += 1;
-                if selected_n >= suggest_lines {
-                    selected_n = 0;
-                }
-            }
-            UiMsg::SelPrev => {
-                selected_n = selected_n
-                    .checked_sub(1)
-                    .unwrap_or(max(suggest_lines - 1, 0));
-            }
-            UiMsg::ExpectSuggsFor(term) => {
-                waiting_for_term = Some(term);
-            }
-        };
-        cursor.move_up(suggest_lines as u16 + 1);
-    }
-    cursor.show();
-    cursor.move_left(t_w);
+    SetSuggestions(Suggestions),
+    OnInput(Option<InputEvent>),
 }
